f5d1cfcf120448f9cf24e4739f9b24f56123940c
Zenrieru <nvll.zen@gmail.com>
2025-09-15 13:42:31 +0700

    V1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<div class="background">
+    <slot>
+
+    </slot>
+</div>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        background-color: black;
+        color: white;
+    }
+
+    .background {
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Layout from "./+layout.svelte";
+    import Checklist from "./checklist.svelte";
+    import "../styles/style.css"
+</script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&family=Share+Tech&display=swap" rel="stylesheet">
+
+<Layout>
+    <div class="mainTitle">
+        <h1>SKSD</h1>
+        <h5>Sistem Kerjakan Sebelum Deadline</h5>
+    </div>
+    <div class="tasks">
+        <div class="taskEntry">
+            <h1 class="taskName taskEntryItems">Tugas 1</h1>
+            <h2 class="taskEntryItems">HH:BB:TT</h2>
+            <h2 class="taskEntryItems">DD:MM:JJ</h2>
+            <button class="taskEntryItems">
+                <Checklist>
+                </Checklist>
+            </button>
+            
+        </div>
+    </div>
+</Layout>
